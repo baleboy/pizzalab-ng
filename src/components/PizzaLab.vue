8a67d0458e473dacf7627529e53e2b951def083d
Francesco Balestrieri <francesco.balestrieri@intel.com>
2018-03-05 11:32:48 +0200

    Fix: adding a new dough was replacing the last edited dough in the main view

--- src/components/PizzaLab.vue
+++ src/components/PizzaLab.vue
@@ -56,7 +56,7 @@ export default {
       adding: false,
       editing: false,
       showRecipe: false,
-      draftDough: new Dough(),
+      draftDough: { key: null, dough: new Dough() },
       doughInEditor: { key: null, dough: new Dough() },
       doughList: [],
       userId: null
@@ -68,7 +68,7 @@ export default {
   },
   methods: {
     addDough: function () {
-      this.doughInEditor.dough = this.draftDough
+      this.doughInEditor = this.draftDough
       this.adding = true
     },
     doughEdited: function () {
